bf8aadf094edda5703d825922e6f9e8b340510be
Zebaanikanibir <zanikanibir@gmail.com>
2021-05-02 21:42:28 +0600

    done5

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import Header from '../Header/Header';
-import Sidebar from '../Sidebar/Sidebar';
+import './Blogs.css';
 const Blogs = () => {
     return (
         <div className="wedding blogs1">
          <Header></Header>
          
             <div className="blog">
-                <h2 className="pHead">MT BLOGS</h2>
-                <p className="text-white mt-5 blog">i am a junior front end web developer.able to build a website with HTML, CSS,javaScript & reactJs.Skilled at writing well designed,testable & efficient code.ok..So first of all I want to become a skilled web developer in future because web development is first paced, challenging career centered on creative problem-solving & continuous innovation…</p>
-                <p className="text-white blog">As a web developer i have the opportunity to work for startups,agencies, large corporates even freelance.So it means i may able to work remotely anywhere in the world</p>
-                <a href="https://zanikanibir.medium.com/" target="_blank" rel="noopener noreferrer"><button id="button">Visit</button></a>
+                <h2 className="pHead slideRight">MT BLOGS</h2>
+                <p className="text-white mt-5 blog slideLeft">i am a junior front end web developer.able to build a website with HTML, CSS,javaScript & reactJs.Skilled at writing well designed,testable & efficient code.ok..So first of all I want to become a skilled web developer in future because web development is first paced, challenging career centered on creative problem-solving & continuous innovation…</p>
+                <p className="text-white blog slideLeft">As a web developer i have the opportunity to work for startups,agencies, large corporates even freelance.So it means i may able to work remotely anywhere in the world</p>
+                <a href="https://zanikanibir.medium.com/" target="_blank" rel="noopener noreferrer"><button className="slideRight" id="button">Visit</button></a>
             </div>
          
         </div>
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -8,14 +8,14 @@ const Home = () => {
             <Header></Header>
             <div className="row justify-content-center align-items-center">
                 <div className="col-md-12 hDiv">
-                    <h2 className="text-white">HI,🙋‍♀️</h2>
+                    <h2 className="text-white slideUp">HI,🙋‍♀️</h2>
                     
-                    {/* <img style={{width: "200px"}} src={image} alt=""/> */}
-                    <h6 className="text-white">I am Junior Web developer able to build a Web presence from the ground up - from concept,</h6>
-                    <h6 className="text-white">navigation, layout and programming to UX and SEO. Skilled at writing well-designed, testable and efficient code using current best practices in Web development. </h6>
-                    <h6 className="text-white">Fast learner, hard worker and team player who is proficient in an array of scripting languages and multimedia Web tools.</h6>
+                    
+                    <h6 className="text-white slideUp">I am Junior Web developer able to build a Web presence from the ground up - from concept,</h6>
+                    <h6 className="text-white slideUp">navigation, layout and programming to UX and SEO. Skilled at writing well-designed, testable and efficient code using current best practices in Web development. </h6>
+                    <h6 className="text-white slideUp">Fast learner, hard worker and team player who is proficient in an array of scripting languages and multimedia Web tools.</h6>
 
-                    <button className="button">MY PORTFOLIO</button>
+                    <button className="button slideIn">MY PORTFOLIO</button>
 
                 </div>
             </div>
