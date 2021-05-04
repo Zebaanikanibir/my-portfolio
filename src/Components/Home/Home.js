94c55d523c078b263dc6d501de37100791851d76
Zebaanikanibir <zanikanibir@gmail.com>
2021-05-04 17:15:29 +0600

    last10

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,15 +2,112 @@ import React from 'react';
 import './Home.css';
 import Header from '../Header/Header';
 import image from '../../images/PicsArt_11-06-01.15.34-01.jpeg'
+import { $ }  from 'react-jquery-plugin'
+import barba from '@barba/core';
+import gsap from 'gsap/gsap-core';
+
+
 const Home = () => {
+    
+    function transitionAnimation(){
+        gsap.to(".loader-overlay.one",{
+        
+            duration:1,
+            scaleX:1,
+            transformOrigin: "left",
+            ease: "power1.inOut"
+        
+        });
+        gsap.to(".loader-overlay.one",{
+        
+            duration:1,
+            scaleX:0,
+            transformOrigin: "right",
+            ease: "power1.inOut",
+            delay:2
+        
+        });
+        gsap.to(".loader-overlay.two",{
+        
+            duration:1.4,
+            scaleX:1,
+            transformOrigin: "left",
+            ease: "power1.inOut",
+            
+        
+        });
+        gsap.to(".loader-overlay.two",{
+        
+            duration:1.4,
+            scaleX:0,
+            transformOrigin: "right",
+            ease: "power1.inOut",
+            delay: 1.6
+        
+        });
+        
+        
+        }
+
+        function delay(n){
+            n = n || 4000;
+            return new Promise((done) =>{
+            
+            
+                setTimeout(() => {
+            
+                    done();
+                }, n)
+            })
+            
+            
+            }
+
+            $(function(){
+
+                barba.init({
+                    sync: true,
+                    transitions: [{
+                        
+                      name: 'default-transition',
+                     async leave(data) {
+                        // create your stunning leave animation here
+                        const done = this.async()
+            
+                       
+                        setTimeout(function(){
+                            transitionAnimation()
+                        },2000)
+                        
+                        await delay(3000);
+                        done()
+                      },
+                     async enter(data) {
+                        // create your amazing enter animation here
+                      }
+                    }]
+                  });
+            
+            })
+
+
+
+
+
     return (
-        <div className="home">
-            <Header></Header>
+        <div className="home" data-barba="wrapper">
+
+            <div className="loader-overlay one"></div>
+            <div className="loader-overlay two"></div>
+
+           <div className="wrapper" data-barba="container" data-barba-namespace="home">
+           <div>
+           <Header></Header>
             <div className="row justify-content-center align-items-center">
                 <div className="col-md-12 hDiv">
                     <h2 className="text-white slideUp">HI,🙋‍♀️</h2>
-                    
-                    
+
+
                     <h6 className="text-white slideUp">I am Junior Web developer able to build a Web presence from the ground up - from concept,</h6>
                     <h6 className="text-white slideUp">navigation, layout and programming to UX and SEO. Skilled at writing well-designed, testable and efficient code using current best practices in Web development. </h6>
                     <h6 className="text-white slideUp">Fast learner, hard worker and team player who is proficient in an array of scripting languages and multimedia Web tools.</h6>
@@ -19,6 +116,9 @@ const Home = () => {
 
                 </div>
             </div>
+           </div>
+
+           </div>
         </div>
     );
 };
--- src/Components/Resume/Resume.js
+++ src/Components/Resume/Resume.js
@@ -5,18 +5,106 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowDown } from '@fortawesome/free-solid-svg-icons'
 import './Resume.css';
 import Header from '../Header/Header';
-
+import { $ }  from 'react-jquery-plugin'
+import barba from '@barba/core';
+import gsap from 'gsap/gsap-core';
 
 
 const Resume = () => {
+    function transitionAnimation(){
+        gsap.to(".loader-overlay.one",{
+        
+            duration:1,
+            scaleX:1,
+            transformOrigin: "left",
+            ease: "power1.inOut"
+        
+        });
+        gsap.to(".loader-overlay.one",{
+        
+            duration:1,
+            scaleX:0,
+            transformOrigin: "right",
+            ease: "power1.inOut",
+            delay:2
+        
+        });
+        gsap.to(".loader-overlay.two",{
+        
+            duration:1.4,
+            scaleX:1,
+            transformOrigin: "left",
+            ease: "power1.inOut",
+            
+        
+        });
+        gsap.to(".loader-overlay.two",{
+        
+            duration:1.4,
+            scaleX:0,
+            transformOrigin: "right",
+            ease: "power1.inOut",
+            delay: 1.6
+        
+        });
+        
+        
+        }
+
+        function delay(n){
+            n = n || 4000;
+            return new Promise((done) =>{
+            
+            
+                setTimeout(() => {
+            
+                    done();
+                }, n)
+            })
+            
+            
+            }
+
+            $(function(){
+
+                barba.init({
+                    sync: true,
+                    transitions: [{
+                        
+                      name: 'default-transition',
+                     async leave(data) {
+                        // create your stunning leave animation here
+                        const done = this.async()
+            
+                       
+                        setTimeout(function(){
+                            transitionAnimation()
+                        },2000)
+                        
+                        await delay(3000);
+                        done()
+                      },
+                     async enter(data) {
+                        // create your amazing enter animation here
+                      }
+                    }]
+                  });
+            
+            })
+
     return (
-        <div className="wedding">
-            <Header></Header>
+        <div className="wedding" data-barba="wrapper">
+            <div className="loader-overlay one"></div>
+            <div className="loader-overlay two"></div>
+        <div className="wrapper" data-barba="container" data-barba-namespace="home">
+            
+       <div>
+       <Header></Header>
             <div className="resume">
           <h1 className="pHead mt-5">MY RESUME</h1>
             <div className="row container-fluid align-items-center mt-5">
                 <div className="col-md-6">
-                <img className="slideLeft" style={{width: '200px'}} src={image} alt=""/>
+                <img className="slideLeft img-fluid" style={{width: '200px'}} src={image} alt=""/>
                 </div>
                 <div className="col-md-6">
                 <h6 className="text-white slideRight">As a junior web developer I am very much dedicated in front end development. Front end development
@@ -34,6 +122,9 @@ const Resume = () => {
 
 
         </div>
+       </div>
+        </div>
+         
         </div>
     );
 };
